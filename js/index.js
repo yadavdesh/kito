746645025ccc753fd0dd983cba092164305332b8
Desh Bhardwaj <deshbhardwaj@Deshs-Air.fritz.box>
2018-06-16 20:49:23 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,21 @@
+        
+
+        function ingredientsHover() {
+            document.getElementById('ingredients').firstElementChild.firstElementChild.style.fontSize = '300%';
+        }
+
+
+        function ingredientsNormal() {
+            document.getElementById('ingredients').firstElementChild.firstElementChild.style.fontSize = '100%';
+            // document.write("Normal");  
+        }
+
+
+        function preparationHover() {
+            document.getElementById('preparation').firstElementChild.firstElementChild.style.fontSize = '300%';
+        }
+
+
+        function preparationNormal() {
+            document.getElementById('preparation').firstElementChild.firstElementChild.style.fontSize = '100%';
+        }
